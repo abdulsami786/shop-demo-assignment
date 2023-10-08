60a6480cd6d9d8c391d7c0cfcbf918c61fd54059
abdulsami786 <abdulsami786125@gmail.com>
2023-10-09 00:23:55 +0500

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-svg-core/styles.css"; 
 
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false; 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -16,7 +20,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+     
       <body className={inter.className}>{children}</body>
+      
+      
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,193 @@
+'use client'
 import Image from 'next/image'
+import React, { useEffect, useState } from 'react';
+
+interface Product {
+  brand: string;
+  category: string;
+  discount: number;
+  imageUrl: string;
+  name: string;
+  newPrice: number;
+  oldPrice: number;
+  productID: string;
+  subCategory: string;
+  subSubCategory: string;
+  url: string;
+}
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+  const ProductCard = ({ product }:{product:Product}) => (
+    <div className="relative m-2 md:m-5 flex max-w-[18rem] max-h-[39rem]  md:max-w-1/2 flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md">
+      <a className="relative mx-3 mt-3 flex h-90 overflow-hidden rounded-xl" href="#">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          className="object-cover"
+          src={product.imageUrl}
+          width="330"
+          height="495"
+          alt="product image"
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
+        <span className="absolute top-0 left-0 m-2 rounded-full bg-black px-2 text-center text-sm font-medium text-white">
+          {product.discount}% OFF
+        </span>
+      </a>
+      <div className="mt-4 px-5 pb-4">
+        <a href="#">
+          <h5 className="no-underline text-sm tracking-tight text-slate-900 ">
+            {product.name}
+          </h5>
         </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+        <div className="mt-2 mb-2 flex items-center justify-between">
+          <p>
+            <span className="text-xl font-bold text-slate-900">
+              ${product.newPrice}
             </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+            <span className="ml-2 text-sm text-slate-900 line-through">
+              ${product.oldPrice}
             </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
           </p>
-        </a>
-
+         
+        </div>
         <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="#"
+          className="flex no-underline items-center justify-center rounded-md bg-slate-900 px-5 py-2 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
+          
+          Add to cart
         </a>
       </div>
-    </main>
-  )
+    </div>
+  );
+
+  const [products, setProducts] = useState<Product[]>([]);
+  const [sortOrder, setSortOrder] = useState('asc'); 
+  const [sortOrder1, setSortOrder1] = useState<'asc' | 'desc'>('asc'); 
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const recordsPerPage = 12;
+  const indexOfLastRecord = currentPage * recordsPerPage;
+  const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
+  const currentRecords = products.slice(indexOfFirstRecord, indexOfLastRecord);
+    
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  
+    fetch('https://shop2-a65de-default-rtdb.asia-southeast1.firebasedatabase.app/products.json')
+      .then((response) => response.json())
+      .then((data) => {
+      
+        setProducts(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data: ', error);
+      });
+  }, []);
+
+  const handleSort = () => {
+    
+    const newSortOrder = sortOrder === 'asc' ? 'desc' : 'asc';
+
+    // Sort the products based on the price
+    const sortedProducts = [...products].sort((a, b) => {
+      if (newSortOrder === 'asc') {
+        return a.newPrice - b.newPrice;
+      } else {
+        return b.newPrice - a.newPrice;
+      }
+    });
+
+
+    setProducts(sortedProducts);
+    setSortOrder(newSortOrder);
+  };
+
+  const handleSortByDiscount = () => {
+    const newSortOrder = sortOrder1 === 'asc' ? 'desc' : 'asc';
+  
+    const sortedProducts = [...products].sort((a, b) => {
+      if (newSortOrder === 'asc') {
+        return a.discount - b.discount;
+      } else {
+        return b.discount - a.discount;
+      }
+    });
+  
+    setProducts(sortedProducts);
+    setSortOrder1(newSortOrder);
+  };
+
+  const nextPage = () => {
+    if (currentPage < Math.ceil(products.length / recordsPerPage)) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+  
+  const prevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+  
+// Function to handle changing the current page when a page number is clicked
+const changePage = (pageNumber: number) => {
+  if (pageNumber >= 1 && pageNumber <= Math.ceil(products.length / recordsPerPage)) {
+    setCurrentPage(pageNumber);
+  }
+};
+// Calculate the range of page numbers to display (6 pages at a time)
+const totalPages = Math.ceil(products.length / recordsPerPage);
+const pageRange = 6;
+const startPage = Math.max(1, currentPage - Math.floor(pageRange / 2));
+const endPage = Math.min(totalPages, startPage + pageRange - 1);
+
+return (
+
+<div className="container mx-auto mt-2">
+      <h1 className="text-xl font-semibold mb-5">Product List</h1>
+      <button className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2   " onClick={handleSort}>
+         Sort by Price {sortOrder === 'asc' ? 'High' : 'Low'}
+       </button>
+       <button className="text-white bg-blue-700 hover:bg-blue-800   font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2" onClick={handleSortByDiscount}>
+     Sort by Discount {sortOrder1 === 'asc' ? 'High' : 'Low'}
+       </button>
+
+      <div className="flex flex-wrap -mx-2">
+        {currentRecords.map((product: any, index) => (
+          <ProductCard key={index} product={product} />
+        ))}
+      </div>
+      <div className="mt-5 flex justify-between">
+        <button
+          onClick={prevPage}
+          disabled={currentPage === 1}
+          className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded"
+        >
+          Previous Page
+        </button>
+        <div className="mb-1 flex justify-center">
+        {/* Generate and render page numbers */}
+        {Array.from({ length: endPage - startPage + 1 }, (_, index) => (
+          <button
+            key={startPage + index}
+            onClick={() => changePage(startPage + index)}
+            className={`mx-1 px-3 py-1 rounded-md text-sm ${currentPage === startPage + index ? 'bg-blue-500 text-white' : 'hover:bg-blue-500 hover:text-white bg-gray-200 text-gray-700'} focus:outline-none`}
+          >
+            {startPage + index}
+          </button>
+        ))}
+      </div>
+        <button
+          onClick={nextPage}
+          disabled={currentPage === Math.ceil(products.length / recordsPerPage)}
+          className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded"
+        >
+          Next Page
+        </button>
+      </div>
+  
+    </div>
+    
+  );
 }
+  
+
